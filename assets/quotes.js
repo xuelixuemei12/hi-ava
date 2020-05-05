53bf06d266197ebe72efad3b808a3aa4590044df
xuelixuemei12 <beraskotor7@gmail.com>
2020-05-05 20:14:34 +0700

    TRIIIIIIIIIIIIII

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,7 +1,7 @@
 // prettier-ignore
 export default [
     { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "antjedtta_", quotes: "umiiii \n semnagat terus puasanya" },
     { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
     { author: "standinshd", quotes: "I love you." },
     { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["umiii", "embebs UWU "],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
